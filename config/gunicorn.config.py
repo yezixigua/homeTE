719f465f1cdfb65d8027c3c3e2dc93314c008ed1
yezixigua <541889983@qq.com>
2018-11-23 16:01:06 +0800

    delete risk

--- config/gunicorn.config.py
+++ config/gunicorn.config.py
@@ -1,2 +1,2 @@
 bind = '0.0.0.0:5000'
-pid = '/tmp/mySite.pid'
+pid = '/tmp/myHome.pid'
--- config/run.sh
+++ config/run.sh
@@ -4,12 +4,12 @@ service supervisor stop
 service nginx stop
 
 # 删除原本建立的软连接
-rm /etc/supervisor/conf.d/mySite.conf
-rm /etc/nginx/sites-enabled/mySite
+rm /etc/supervisor/conf.d/myHome.conf
+rm /etc/nginx/sites-enabled/myHome
 
 # 建立对应软件的配置软连接，将配置源文件放在项目本地，在对应软件目录下生成链接，链接到本地
-ln -s /home/an/gitcode/mySite/config/supervisor.conf /etc/supervisor/conf.d/mySite.conf
-ln -s /home/an/gitcode/mySite/config/nginx_config /etc/nginx/sites-enabled/mySite
+ln -s /home/an/myHome/config/supervisor.conf /etc/supervisor/conf.d/myHome.conf
+ln -s /home/an/myHome/config/nginx_config /etc/nginx/sites-enabled/myHome
 
 # 重启服务
 service supervisor restart
